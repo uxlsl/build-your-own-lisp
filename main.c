20d7ec220081be0bd089bd66b4f35eb960c3dac0
lin.sl <lin.sl.0001@gmail.com>
2021-01-20 19:36:36 +0800

    增加语法分析

--- main.c
+++ main.c
@@ -4,28 +4,55 @@
 #include <editline/readline.h>
 #include <editline/history.h>
 
+#include "mpc.h"
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
+
+
+  mpc_parser_t * Number = mpc_new("number");
+  mpc_parser_t * Operator = mpc_new("operator");
+  mpc_parser_t * Expr = mpc_new("expr");
+  mpc_parser_t * Lispy = mpc_new("lispy");
+
+  mpca_lang(MPCA_LANG_DEFAULT,
+    "                                                     \
+      number   : /-?[0-9]+/ ;                             \
+      operator : '+' | '-' | '*' | '/' ;                  \
+      expr     : <number> | '(' <operator> <expr>+ ')' ;  \
+      lispy    : /^/ <operator> <expr>+ /$/ ;             \
+    ",
+    Number, Operator, Expr, Lispy);
 
   /* Print Version and Exit Information */
   puts("Lispy Version 0.0.0.0.1");
   puts("Press Ctrl+c to Exit\n");
 
   /* In a never ending loop */
-  while (1) {
+  while (1)
+  {
 
     /* Output our prompt and get input */
-    char* input = readline("lispy> ");
+    char *input = readline("lispy> ");
 
     /* Add input to history */
     add_history(input);
 
     /* Echo input back to user */
     printf("No you're a %s\n", input);
-
+   /* Attempt to parse the user input */
+    mpc_result_t r;
+    if (mpc_parse("<stdin>", input, Lispy, &r)) {
+      /* On success print and delete the AST */
+      mpc_ast_print(r.output);
+      mpc_ast_delete(r.output);
+    } else {
+      /* Otherwise print and delete the Error */
+      mpc_err_print(r.error);
+      mpc_err_delete(r.error);
+    }
     /* Free retrieved input */
     free(input);
-
   }
 
   return 0;
